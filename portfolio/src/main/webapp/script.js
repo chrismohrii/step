5aaba64c1dedb13d7e96fb6912d54aacf8434176
chrismohrii <65875732+chrismohrii@users.noreply.github.com>
2020-06-08 18:43:31 -0400

    Add maps (#6)
    * Fix code style slightly
    
    * Add map to home page
    
    * Add more markers
    
    * Add info windows
    
    * Add more info in markers
    
    * Fix indentation
    
    * Fix lucas comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,125 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const iconBeachFlag = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+const mapStyle = [
+  {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+  {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+  {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#c9b2a6'}]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#dcd2be'}]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#ae9e90'}]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#93817c'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [{color: '#a5b076'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#447530'}]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{color: '#f5f1e6'}]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [{color: '#fdfcf8'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{color: '#f8c967'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#e9bc62'}]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [{color: '#e98d58'}]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#db8555'}]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#806b63'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#8f7d77'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.stroke',
+    stylers: [{color: '#ebe3cd'}]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [{color: '#b9d3c2'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#92998d'}]
+  }]
+
+/**
+ * Calls the methods that load the home page.
+ */
+function start() {
+  getCommentSection();
+  createMap();
+}
+
 /**
  * Adds a random greeting to the page.
  */
@@ -65,9 +184,9 @@ function addRandomNumber(container){
 */
 function hasWon(){
   const winBox = document.getElementById('win-container');
-  var firstNumber = document.getElementById('number1-container').innerText;
-  var secondNumber = document.getElementById('number2-container').innerText;
-  var thirdNumber = document.getElementById('number3-container').innerText;
+  const firstNumber = document.getElementById('number1-container').innerText;
+  const secondNumber = document.getElementById('number2-container').innerText;
+  const thirdNumber = document.getElementById('number3-container').innerText;
 
   // Compare the numbers for appropriate message. 
   if (firstNumber === secondNumber && firstNumber == thirdNumber){
@@ -137,3 +256,33 @@ function deleteSpecificComment(comment) {
   fetch('/delete-given-comment', {method: 'POST', body: params});
 }
 
+/** Creates a map and adds it to the page. */
+async function createMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.4500, lng: -76.4800},
+    zoom: 15.2,
+    styles: mapStyle
+    }
+  );
+ 
+  addMarker(map, {lat: 42.4550, lng: -76.4777}, "Freshman year dorm", iconBeachFlag, "<b>Mary Donlon Hall</b><br>Freshman year dorm - Chris lived on the fourth floor in a quad");
+  addMarker(map, {lat: 42.4477, lng: -76.4853}, "Favorite library", iconBeachFlag, "<b>Uris Library</b><br>The underground is called the Cocktail lounge");
+  addMarker(map, {lat: 42.4462, lng: -76.4823}, "Favorite dining hall", iconBeachFlag, "<b>Terrace dining hall</b><br>Has great food");
+  addMarker(map, {lat: 42.4529, lng: -76.4774}, "Favorite gym", iconBeachFlag, "<b>Helen Newman Gym</b><br>Has gym, basketball courts, and swimming pool");
+}
+
+/** Creates a marker given a map, coordinates, description, and icon. */
+function addMarker(map, coordinates, title, icon, description){
+  const marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: coordinates,
+    title: title,
+    icon: icon
+  });
+
+  const infoWindow = new google.maps.InfoWindow({content: description});
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });	
+}
